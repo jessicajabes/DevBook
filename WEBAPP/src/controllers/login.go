fbb4e857257a007c62999e0cfb42ef4aa24a29e1
jessicajabes <jessicajabes@hotmail.com>
2025-02-02 19:07:42 -0300

    02 02

---
+++ WEBAPP/src/controllers/login.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"webapp/src/respostas"
+)
+
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	usuario, erro := json.Marshal(map[string]string{
+		"email": r.FormValue("email"),
+		"senha": r.FormValue("senha"),
+	})
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:5000/login")
+	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	token, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode, string(token))
+
+}
