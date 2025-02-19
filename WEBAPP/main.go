a793ad06a8924382ddbfe7c5fd588595ac064d05
jessicajabes <jessicajabes@hotmail.com>
2025-02-19 18:39:02 -0300

    19 02

--- WEBAPP/main.go
+++ WEBAPP/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,17 +8,7 @@ import (
 	"webapp/src/cookies"
 	"webapp/src/router"
 	"webapp/src/utils"
-
-	"github.com/gorilla/securecookie"
 )
-
-func init() {
-	hashKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
-	fmt.Println(hashKey)
-
-	blockKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
-	fmt.Println(blockKey)
-}
 
 func main() {
 	config.Carregar()
